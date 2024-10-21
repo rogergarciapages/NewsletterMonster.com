74c9785271021bc15a540fc51c9b121f49cc5022
Roger Garcia <96830104+rogergarciapages@users.noreply.github.com>
2024-10-21 16:18:38 +0100

    styles

--- src/app/components/save-newsletters.tsx
+++ src/app/components/save-newsletters.tsx
@@ -24,34 +24,34 @@ export default function SaveNewsletters() {
         </div>
         <div className="md:col-span-6 order-1 md:order-2 space-y-8 ">
           <h2 className="text-5xl font-bold tracking-tighter">Save Your Favorite Newsletters</h2>
-          <p className="text-base md:text-base lg:text-base xl:text-base dark:text-gray-300">
+          <p className="text-base md:text-base lg:text-base xl:text-base">
             Never miss important updates again. Archive and organize your favorite newsletters in one place. Easily access past issues, highlight key information, and stay on top of 
             your subscriptions.
           </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-4 px-4">
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Increased Visibility</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Gain exposure to a wider audience by showcasing your newsletter on our site, attracting new subscribers, and expanding your reach.</p>
+              <p className="text-sm">Gain exposure to a wider audience by showcasing your newsletter on our site, attracting new subscribers, and expanding your reach.</p>
             </div>
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Huge SEO Advantages</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Improve your search engine rankings with external links from our reputable platform to your domain, enhancing your online presence.</p>
+              <p className="text-sm ">Improve your search engine rankings with external links from our reputable platform to your domain, enhancing your online presence.</p>
             </div>
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Design Recognition</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Highlight your newsletter's design excellence and marketing practices, enhancing your brand's credibility and reputation within the industry.</p>
+              <p className="text-sm">Highlight your newsletter's design excellence and marketing practices, enhancing your brand's credibility and reputation within the industry.</p>
             </div>
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Networking Opportunities</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Connect with other companies and professionals featured on the platform, opening doors to potential collaborations and partnerships.</p>
+              <p className="text-sm">Connect with other companies and professionals featured on the platform, opening doors to potential collaborations and partnerships.</p>
             </div>
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Constructive Feedback</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Receive valuable feedback from our community, helping you refine and improve your newsletter content and strategy.</p>
+              <p className="text-sm">Receive valuable feedback from our community, helping you refine and improve your newsletter content and strategy.</p>
             </div>
             <div className="space-y-2">
               <h3 className="text-xl font-bold tracking-tight leading-[2rem] text-primar border-b-8 border-torch-600">Enhanced Deliverability</h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">Benefit from our focus on best practices, leading to higher engagement rates and improved deliverability of your newsletters to inboxes.</p>
+              <p className="text-sm">Benefit from our focus on best practices, leading to higher engagement rates and improved deliverability of your newsletters to inboxes.</p>
             </div>
           </div>
           <div className="flex flex-col gap-2 sm:flex-row">
